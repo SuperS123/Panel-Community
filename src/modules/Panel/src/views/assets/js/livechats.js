cc4436dce8fa6b47ad050d762ab00104f23b3053
Dominik <55330408+Dom-in@users.noreply.github.com>
2020-12-19 11:58:03 +0000

    Update livechats.js

--- src/modules/Panel/src/views/assets/js/livechats.js
+++ src/modules/Panel/src/views/assets/js/livechats.js
@@ -3,13 +3,13 @@
 let { livechat } = require("../../../../../../../config/core.json");
 
 if (livechat.enabled) {
-    if (livechat.livechatv == 1) {
+    if (livechat.livechatv === 1) {
 
         window.__lc = window.__lc || {};
         window.__lc.license = livechat.api;
         ; (function (n, t, c) { function i(n) { return e._h ? e._h.apply(null, n) : e._q.push(n) } var e = { _q: [], _h: null, _v: "2.0", on: function () { i(["on", c.call(arguments)]) }, once: function () { i(["once", c.call(arguments)]) }, off: function () { i(["off", c.call(arguments)]) }, get: function () { if (!e._h) throw new Error("[LiveChatWidget] You can't use getters before load."); return i(["get", c.call(arguments)]) }, call: function () { i(["call", c.call(arguments)]) }, init: function () { var n = t.createElement("script"); n.async = !0, n.type = "text/javascript", n.src = "https://cdn.livechatinc.com/tracking.js", t.head.appendChild(n) } }; !n.__lc.asyncInit && e.init(), n.LiveChatWidget = n.LiveChatWidget || e }(window, document, [].slice))
 
-    } else if (livechat.livechatv == 2) {
+    } else if (livechat.livechatv === 2) {
 
         (function (d, w, c) {
             w.ChatraID = livechat.api;
@@ -22,7 +22,7 @@ if (livechat.enabled) {
             if (d.head) d.head.appendChild(s);
         })(document, window, 'Chatra');
 
-    } else if (livechat.livechatv == 3) {
+    } else if (livechat.livechatv === 3) {
 
         (function () {
             var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
@@ -33,7 +33,7 @@ if (livechat.enabled) {
             s0.parentNode.insertBefore(s1, s0);
         })();
 
-    } else if (livechat.livechatv == 4) {
+    } else if (livechat.livechatv === 4) {
 
         let d = document
         let s = d.createElement("script");
